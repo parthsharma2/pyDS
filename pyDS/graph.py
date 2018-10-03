e9b174c577647e93fbc4df8d0f97e8406aa3a0a9
Dhananjai Sharma <sharma.dj1998@gmail.com>
2018-10-03 18:16:10 +0530

    Update graph.py

--- pyDS/graph.py
+++ pyDS/graph.py
@@ -1,28 +1,38 @@
 from collections import defaultdict 
-# defaultdict will make news keys if key does not exist
 
 class Graph():
 	"""An implementation of graph data structure."""
+	
 	def __init__(self):
+		"""Initialize the graph."""
+		
 		self._edges = []
 		self._graph = defaultdict(list) 
 
 	def __len__(self):
+		"""Return the size of the Graph."""
+		
 		return (len(self._edges))
 
 	def __str__(self):
-		"""Return the string representation of the Graph."""
+		"""Return the informal string representation of the Graph."""
+		
 		return str(self._items)
 
 	def __repr__(self):
+		"""Return the formal string representation of the Graph."""
+		
 		return "pyDS.graph.Graph({})".format(self._graph)
 
-	def addEdge(self,u,v):
+	def add_edge(self,u,v):
+		"""Return the graph after adding the edge."""
+		
 		self._graph[u].append(v)
 		return (self._graph) 
 
-	def generateEdges(self):
-		"""Returns all the edges in tuple inside a list. Ex - [(a,b),(c,d)]"""
+	def generate_edges(self):
+		"""Returns all the edges in tuple inside a list."""
+		
 		edges = []
 		for node in self._graph:
 			for neighbour in self._graph[node]:
